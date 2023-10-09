f0589624afcac8b02d3f2035f68e31af2bb5a64c
daviardev <jerson.silva99@hotmail.com>
2023-10-08 23:26:31 -0500

    Using main layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,13 @@
+'use client'
+
+import MainLayout from './Layouts/MainLayout'
+
 export default function Home () {
   return (
-    <></>
+    <>
+      <MainLayout>
+        Inicio
+      </MainLayout>
+    </>
   )
 }
