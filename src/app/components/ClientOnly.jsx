34de1c959aaad16b72be11b12403034aa6e8f208
daviardev <jerson.silva99@hotmail.com>
2023-10-08 23:27:06 -0500

    Create component for use client

---
+++ src/app/components/ClientOnly.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+export default function CLientOnly ({ children }) {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  return (
+    isClient
+      ? <div>{children}</div>
+      : null
+  )
+}
