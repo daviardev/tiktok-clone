e80f7cc73c059a91d38e04c02ba9375d789ad6ca
daviardev <jerson.silva99@hotmail.com>
2023-10-12 21:45:56 -0500

    Use sidebar component in layout

--- src/app/layouts/MainLayout.jsx
+++ src/app/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { usePathname } from 'next/navigation'
 import NavBar from './includes/NavBar'
+import Sidebar from '../components/Sidebar'
 
 export default function MainLayout ({ children }) {
   const path = usePathname()
   return (
     <>
       <NavBar />
-      <div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${path === '/' ? 'max-w-[1140px]' : ''}`}>
+      <div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${path === '/' ? 'max-w-full' : ''}`}>
+        <Sidebar />
         {children}
       </div>
     </>
