9e30f19b9d5d2e510558824fc971db3dfd7e45fd
daviardev <jerson.silva99@hotmail.com>
2023-10-16 01:18:58 -0500

    Using component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,26 @@
 'use client'
 
+import PostMain from './components/PostMain'
 import MainLayout from './Layouts/MainLayout'
+import ClientOnly from './components/ClientOnly'
 
 export default function Home () {
   return (
     <>
       <MainLayout>
-        Inicio
+        <div className='mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto'>
+          <ClientOnly>
+            <PostMain
+              id='123'
+              name='daviardev'
+              avatar='/placeholder-user.jpg'
+              uderId='456'
+              title='Componentes para una PC GAMER de $7,000 #pcgamer #gaming #gamingpc'
+              videoUrl='/video.mp4'
+              createdAt='Thu Oct 12 2023 21:58:26 GMT-0500 (hora estándar de Colombia)'
+            />
+          </ClientOnly>
+        </div>
       </MainLayout>
     </>
   )
