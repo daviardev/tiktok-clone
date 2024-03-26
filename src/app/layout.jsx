ef83923c60e9b4f7d2617c08aaffe9c3c69cd1c2
daviardev <jerson.silva99@hotmail.com>
2024-03-26 16:38:02 -0500

    Change layout app

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
-import MainLayout from './components/MainLayout'
 import './globals.css'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 export const metadata = {
   title: 'TikTok - Make Your Day',
@@ -10,9 +12,20 @@ export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
       <body>
-        <MainLayout>
-          {children}
-        </MainLayout>
+        <div className='shadow-filter'>
+          <main className='flex bg-[url(/images/iphone.webp)] bg-no-repeat bg-contain w-[320px] h-[640px] relative'>
+            <section className='w-full rounded-[32px] flex gap-3 flex-col overflow-hidden relative px-1.5 py-4 m-6'>
+              <div className='absolute bg-white inset-0 -z-10' />
+              <Header />
+              <div className='relative w-full h-full mx-auto my-0'>
+                <article className='overflow-hidden absolute inset-0 w-full h-full z-20'>
+                  {children}
+                </article>
+              </div>
+              <Footer />
+            </section>
+          </main>
+        </div>
       </body>
     </html>
   )
